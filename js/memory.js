a61ead0938d6617b9ab7cb4629e99f6a1a810fb9
u1980975 <u1980975@campus.udg.edu>
2024-03-05 03:59:54 +0100

    intentando cosas

--- js/memory.js
+++ js/memory.js
@@ -21,16 +21,35 @@ export var game = function(){
     var lastCard;
     var pairs = 2;
     var points = 100;
+    var items = [];
 
     return {
+        
         init: function (call){
-            var items = resources.slice(); // Copiem l'array
+
+            items = resources.slice(); // Copiem l'array
             items.sort(() => Math.random() - 0.5); // Aleatòria
             items = items.slice(0, pairs); // Agafem els primers
             items = items.concat(items);
             items.sort(() => Math.random() - 0.5); // Aleatòria
+
+            items.forEach((item, index) => {
+                const currentItem = Object.create(card, {front: {value: item}, callback: {value: call}});
+                setTimeout(() => {
+                    currentItem.goFront(); // Mostrar la carta
+                    currentItem.goBack(); // Ocultar la carta después de un segundo
+                }, (index + 1) * 1000); // Ajustamos el tiempo para que cada carta se muestre por un segundo
+            });
+        
+            // Después de que todas las cartas se muestren, llamamos a la función call para continuar con el juego
+            setTimeout(call, (items.length + 1) * 1000);
+        
+
+
             return items.map(item => Object.create(card, {front: {value:item}, callback: {value:call}}));
+
         },
+
         click: function (card){
             if (!card.clickable) return;
             card.goFront();

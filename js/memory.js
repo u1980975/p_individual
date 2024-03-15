15347a7085044cc8707ffe801777f4865b461b54
u1980975 <u1980975@campus.udg.edu>
2024-03-15 19:25:51 +0100

    Memory acabado

--- js/memory.js
+++ js/memory.js
@@ -15,38 +15,48 @@ export var game = function(){
             this.current = this.front;
             this.clickable = false;
             this.callback();
+        },
+        goBackEasy: function() {
+            setTimeout(() => {
+                this.current = back;
+                this.clickable = true;
+                this.callback();
+            }, 2000);
+        },
+        goBackHard: function() {
+            setTimeout(() => {
+                this.current = back;
+                this.clickable = true;
+                this.callback();
+            }, 500);
         }
     };
 
+    var options = JSON.parse(localStorage.options)
     var lastCard;
-    var pairs = 2;
+    var pairs = options.pairs;
     var points = 100;
-    var items = [];
 
     return {
         
         init: function (call){
-
-            items = resources.slice(); // Copiem l'array
+            var items = resources.slice(); // Copiem l'array
             items.sort(() => Math.random() - 0.5); // Aleatòria
             items = items.slice(0, pairs); // Agafem els primers
             items = items.concat(items);
             items.sort(() => Math.random() - 0.5); // Aleatòria
 
-            items.forEach((item, index) => {
-                const currentItem = Object.create(card, {front: {value: item}, callback: {value: call}});
-                setTimeout(() => {
-                    currentItem.goFront(); // Mostrar la carta
-                    currentItem.goBack(); // Ocultar la carta después de un segundo
-                }, (index + 1) * 1000); // Ajustamos el tiempo para que cada carta se muestre por un segundo
-            });
-        
-            // Después de que todas las cartas se muestren, llamamos a la función call para continuar con el juego
-            setTimeout(call, (items.length + 1) * 1000);
-        
+            var currentItem = items.map(item => Object.create(card, {front: {value:item}, callback: {value:call}}));
 
 
-            return items.map(item => Object.create(card, {front: {value:item}, callback: {value:call}}));
+            for (var i = 0; i < currentItem.length; i++) {
+                currentItem[i].pointer = $('#c'+i);
+                currentItem[i].pointer.attr("src", currentItem[i].current);
+                currentItem[i].goFront();
+                currentItem[i].goBack();
+            }
+
+            return currentItem;
 
         },
 
@@ -62,8 +72,29 @@ export var game = function(){
                     }
                 }
                 else{
-                    [card, lastCard].forEach(c=>c.goBack());
-                    points-=25;
+
+                    if (options.difficulty == "easy") {
+                        [card, lastCard].forEach(c=>c.goBackEasy());
+
+                       }else if (options.difficulty == "normal") {
+                        [card, lastCard].forEach(c=>c.goBack());
+
+                       }else if (options.difficulty == "hard") {
+                        [card, lastCard].forEach(c=>c.goBackHard());
+                       }
+
+
+                    if (options.difficulty == "easy") {
+                        points-=10;
+
+                    }else if (options.difficulty == "normal") {
+                        points-=25;
+
+                    }else if (options.difficulty == "hard") {
+                        points-=50;
+
+                    }
+
                     if (points <= 0){
                         alert ("Has perdut");
                         window.location.replace("../");
